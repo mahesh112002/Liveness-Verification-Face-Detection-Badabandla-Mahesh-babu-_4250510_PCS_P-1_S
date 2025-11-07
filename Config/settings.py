8617b0f000916b04998078cdf40a3f03a53d805b
maheshbabubadabandla <mahesh-babu.badabandla@iu.org>
2025-11-07 11:15:13 +0100

    inital updating the git folder

---
+++ Config/settings.py
@@ -0,0 +1,19 @@
+
+import os
+
+class Settings:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # Paths
+    REFERENCE_DIR = os.path.join(BASE_DIR, "data", "reference")
+    REFERENCE_PATHS = [os.path.join(REFERENCE_DIR, "reference.jpg")]
+    LOG_DIR = os.path.join(BASE_DIR, "data", "logs")
+
+    # DeepFace configuration
+    MODEL_NAME = "ArcFace"
+    DETECTOR_BACKENDS = ["retinaface", "mtcnn", "opencv"]
+    COSINE_THRESHOLD = 0.35
+
+    # Camera configuration
+    FRAME_SKIP = 30
+    CAM_INDEX = 0
